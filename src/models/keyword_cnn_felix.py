4ab51efcee3ed8d8792297138d7666a4f00bbdfe
Felix Stollberger <felix.stollberger@student.tugraz.at>
2021-06-12 18:27:09 +0200

    Update CNN model Felix

--- src/models/keyword_cnn_felix.py
+++ src/models/keyword_cnn_felix.py
@@ -53,12 +53,13 @@ class KeyWordCNN1d(nn.Module):
         if len(x.shape) != 3:
             x = torch.squeeze(x)
             
-        batch_size = x.shape[0]   # depends
-        in_channels = x.shape[1]  # 40 
-        signal_length = x.shape[2]# 81
+        batch_size = x.shape[0]    # depends
+        in_channels = x.shape[1]   # 40 
+        signal_length = x.shape[2] # 81
 
         x = torch.relu(self.conv_layer(x))
         x = torch.mean(x,dim=2)
+        #print('Post conv shape: ', x.shape) [256,20]
         x = torch.relu(self.linear(x))
         x = self.output_layer(x)
         output = F.log_softmax(x,dim=1)
@@ -73,6 +74,7 @@ class KeyWordCNN2d(nn.Module):
     """
 
     def __init__(self, num_classes, num_features, num_kernels, mem_depth):
+        
         """
         Parameters
         ----------
@@ -91,8 +93,14 @@ class KeyWordCNN2d(nn.Module):
         self.mem_depth = mem_depth
         self.num_classes = num_classes
         #TODO: define your model here
+        
+        self.conv_layer = nn.Conv2d(1, num_kernels, kernel_size=(num_features, mem_depth), 
+                                    padding=(num_features-1,mem_depth - 1))
+        
+        self.linear = nn.Linear(num_kernels*(2*num_features-1), 256)
+        self.output_layer = nn.Linear(256, 5)
 
-    def forward(self, x):num_groups
+    def forward(self, x):
         """Forward pass of the network.
 
         Parameters
@@ -105,8 +113,29 @@ class KeyWordCNN2d(nn.Module):
         torch.Tensor
             The network output (softmax logits) of shape (batch_size, num_classes)
         """
+          
         #TODO: implement the forward pass here
+        
+        if len(x.shape) != 4:
+            x = torch.unsqueeze(x,0)
+        
+        #print('Input shape: ', x.shape)
+            
+        batch_size = x.shape[0]   # depends
+        in_channels = x.shape[1]  # 40 
+        signal_length = x.shape[3]# Variable
 
-        # output = F.log_softmax(...)
+        x = torch.relu(self.conv_layer(x))
+        x = torch.mean(x,dim=3)
+        #print('Post conv shape 1: ', x.shape)
+        
+        x = torch.flatten(x, start_dim=1)
+        
+        #print('Post conv shape 2: ', x.shape)
+        x = torch.relu(self.linear(x))
+        x = self.output_layer(x)
+        output = F.log_softmax(x,dim=1)
+        
         # return output
-        pass
+        
+        return output.view(batch_size, -1)[:, :signal_length]
